12aa2e4311f6128ee3672a18de4a3123c4f1009d
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-02-22 15:47:00 +0800

    Added answer to code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str,ctr) {
+    let word = ''; 
 
+    if (ctr < 0) return "ERROR";
+
+    for(let i=0;i < ctr;i++){        
+        word += str;
+    }
+    return word;
 };
 
 // Do not edit below this line
