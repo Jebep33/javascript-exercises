f84222fff27de1962b230ba370ac74891b4e0851
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-03-20 17:00:16 +0800

    Added answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(ctr) {
+    currNum = 0;
+    nextNum = 1;
+    result = 0;
+
+    if(ctr<0) return 'OOPS';
+    
+    for(i = 0; i < ctr; i++){
+        result = nextNum + currNum;
+        currNum = nextNum;
+        nextNum = result;
+    }
+    return currNum;
 
 };
 
