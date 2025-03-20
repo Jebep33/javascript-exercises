2cdc1d868272a7bbcc99daac6209095c079a2175
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-03-20 18:00:37 +0800

    Added answers

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortByAge = people
+        .sort((personA,personB) => {
+            
+            if(personA["yearOfDeath"] === undefined){
+                personA["yearOfDeath"] = 2025;
+            }
+            if(personB["yearOfDeath"] === undefined){
+                personB["yearOfDeath"] = 2025;
+            }
 
+            const previousGuy = personA["yearOfDeath"] - personA["yearOfBirth"];
+            const nextGuy = personB["yearOfDeath"] - personB["yearOfBirth"];
+
+            return nextGuy - previousGuy;
+        });
+
+        return sortByAge[0];
 };
 
 // Do not edit below this line
