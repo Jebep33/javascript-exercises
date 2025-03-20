c0ece5af84596d50eeb66a963f7dfc50bd318943
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-03-20 16:11:13 +0800

    Added answers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    let cleanStr = str
+        .toLowerCase()
+        .split('')
+        .filter(char => characters.includes(char))
+        .join('');
+
+    let reverseStr = cleanStr.split('').reverse().join('');
+    
+    return cleanStr === reverseStr;
 
 };
 
